38642043beb19e017a6ad1d7224bcdfb478329e3
si-saaref <achmadsyarif241@gmail.com>
2023-04-27 09:20:39 +0700

    feat: fix styling drawer and navbar

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,12 +2,14 @@ import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/css-starter.css';
 import '../styles/main.css';
 
-document.querySelector('.hamburger-wrapper').addEventListener('click', function () {
-	document.getElementById('drawer').classList.add('active');
+document.querySelector('.hamburger-wrapper').addEventListener('click', function (evt) {
+	evt.stopPropagation();
+	document.querySelector('#drawer').classList.toggle('active');
 });
 
-['.hero', '.main', '.head'].forEach((item) => {
-	document.querySelector(`${item}`).addEventListener('click', function () {
+['.hero', '.main', '.header'].forEach((item) => {
+	document.querySelector(`${item}`).addEventListener('click', function (evt) {
+		evt.stopPropagation();
 		document.getElementById('drawer').classList.remove('active');
 	});
 });
