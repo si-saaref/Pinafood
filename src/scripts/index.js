fbf2efa0e157f15e3137348e6136dee7112897c9
si-saaref <achmadsyarif241@gmail.com>
2023-04-27 12:13:17 +0700

    feat: add box shadow on header

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -20,11 +20,13 @@ document.addEventListener('scroll', function () {
 		document.querySelector('.header h1').style.color = 'black';
 		document.querySelector('.hamburger-wrapper ').style.color = 'black';
 		document.querySelectorAll('.header a').forEach((elem) => (elem.style.color = 'black'));
+		document.querySelector('.header').style.boxShadow = '0 2px 10px 5px rgba(0, 0, 0, 0.2)';
 	} else {
 		document.querySelector('.header').style.backgroundColor = 'transparent';
 		document.querySelector('.header h1').style.color = 'white';
 		document.querySelector('.hamburger-wrapper ').style.color = 'white';
 		document.querySelectorAll('.header a').forEach((elem) => (elem.style.color = 'white'));
+		document.querySelector('.header').style.boxShadow = 'none';
 	}
 });
 
