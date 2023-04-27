9d97de1bbf1d8f486a1643004020590a46428ee6
si-saaref <achmadsyarif241@gmail.com>
2023-04-27 12:09:09 +0700

    feat: fix hero design

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -13,3 +13,26 @@ document.querySelector('.hamburger-wrapper').addEventListener('click', function 
 		document.getElementById('drawer').classList.remove('active');
 	});
 });
+
+document.addEventListener('scroll', function () {
+	if (window.scrollY > 50) {
+		document.querySelector('.header').style.backgroundColor = 'white';
+		document.querySelector('.header h1').style.color = 'black';
+		document.querySelector('.hamburger-wrapper ').style.color = 'black';
+		document.querySelectorAll('.header a').forEach((elem) => (elem.style.color = 'black'));
+	} else {
+		document.querySelector('.header').style.backgroundColor = 'transparent';
+		document.querySelector('.header h1').style.color = 'white';
+		document.querySelector('.hamburger-wrapper ').style.color = 'white';
+		document.querySelectorAll('.header a').forEach((elem) => (elem.style.color = 'white'));
+	}
+});
+
+const mediaQuery = window.matchMedia('(min-width: 768px)');
+function handleCloseDrawerOnDesktop(e) {
+	if (e.matches) {
+		document.querySelector('#drawer').classList.remove('active');
+	}
+}
+mediaQuery.addListener(handleCloseDrawerOnDesktop);
+handleCloseDrawerOnDesktop(mediaQuery);
