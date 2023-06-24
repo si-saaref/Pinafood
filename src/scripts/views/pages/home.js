f7f5fcb84ce8aec0b888d66aa80069ed24548647
si-saaref <achmadsyarif241@gmail.com>
2023-06-24 09:48:04 +0700

    refactor: refactoring homepage to template createor

---
+++ src/scripts/views/pages/home.js
@@ -0,0 +1,16 @@
+const Home = {
+	async render() {
+		return `
+      <section>
+				<div class="content-header">
+					<header class="content-header__label">Explore Restaurant</header>
+				</div>
+				<div class="content-post"></div>
+			</section>
+    `;
+	},
+
+	async afterRender() {},
+};
+
+export default Home;
---
+++ src/scripts/views/templates/template-creator.js
@@ -0,0 +1,26 @@
+export const cardRestoItemTemplate = () => `
+  <article class="post-item" tabindex="0">
+    <img
+      class="post-item__thumbnail"
+      src="https://restaurant-api.dicoding.dev/images/medium/38"
+      alt="Gambar Ducky Duck"
+    />
+    <div class="post-item__content">
+      <p class="post-item__city">Malang</p>
+      <h1 class="post-item__title">Ducky Duck</h1>
+      <p class="post-item__description">
+        But I must explain to you how all this mistaken idea of denouncing pleasure and
+        praising pain was born and I will give you a complete account of the system, and
+        expound the actual teachings of the great explorer of the truth, the master-builder
+        of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it
+        is pleasure, but because those who do not know how to pursue pleasure rationally
+        encounter consequences that are extremely painful. Nor again is there anyone who
+        loves or pursues or desires to obtain pain of itself, because it is pain, but
+        because occasionally circumstances occur in which toil and pain can procure him some
+        great pleasure.
+      </p>
+    </div>
+  </article>
+`;
+
+export const cardRestoItemDetail = () => {};
